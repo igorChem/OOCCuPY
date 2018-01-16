c87758b3e319350df95756d5c209ed3ec63f904d
igorchem <barden.igor@gmail.com>
2018-01-16 16:59:18 -0300

    new files for the orca in/out classes

---
+++ modules/orca_inp.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# orca_inp.py
+
+#======================================================================
+#import modules 
+
+import os
+import xyz_class
+
+#======================================================================
+
+class orca_inp:
+	pass
+
+	
